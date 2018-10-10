0a39326de3e718dd7376984b7ff4cb6130ace983
Daniel Leszczyński <danielxleszczynski@gmail.com>
2018-10-10 12:07:28 +0200

    Change copy

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React & testing GitHub Pages
           </a>
         </header>
       </div>
