b977c5bf25457439f841ad4c585beb0f9206955e
Daniel Leszczyński <danielxleszczynski@gmail.com>
2018-10-10 11:53:53 +0200

    Updates

---
+++ precache-manifest.d7591d3aa6b62316401704d546185fb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coins/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4721eaa5b4002497506b",
+    "url": "/coins/static/js/runtime~main.4721eaa5.js"
+  },
+  {
+    "revision": "2e48747b181327c98e51",
+    "url": "/coins/static/js/main.2e48747b.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/coins/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "2e48747b181327c98e51",
+    "url": "/coins/static/css/main.6c739878.chunk.css"
+  },
+  {
+    "revision": "d5e3fc3a17f143ff769da4d787d84ddf",
+    "url": "/coins/index.html"
+  }
+];
